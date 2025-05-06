342623d48e838e7f3c06b451d6c93a30a5f274e5
sri reactdev <sri.reactdev@gmail.com>
2025-05-06 22:53:36 +0000

    updated till ReactJS Tutorial 6 : Auto Format React Code With Prettier

---
+++ ReactJS-CodeStoic/ReactJS-Tutorial-06-AutoFormatReactCodeWithPrettier/Readme.md
@@ -0,0 +1,18 @@
+Install the Prettier Format Extension
+Open the extension where you find a json object with Prettier settings. 
+as mentioned below.
+![alt text](image.png)
+Copy the block and open vscode settings, open setting as json and save to file
+```
+{
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "[javascript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  }
+}
+```
+After saving to the code, 
+you have reopen the settings and select Format on Save
+![alt text](image-2.png)
+
+
--- ReactJS-CodeStoic/ReactJs-Tutorial-05-Create-A-Component-in-React/react-app-create-component/src/App.jsx
+++ ReactJS-CodeStoic/ReactJs-Tutorial-05-Create-A-Component-in-React/react-app-create-component/src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Hello from './components/Hello'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div className="App">
       <h1>App Component Works</h1>
-      <Hello/>
-      Can be used using single closing tag or Opening and closing Component tag 
+      <Hello />
+      Can be used using single closing tag or Opening and closing Component tag
       <Hello></Hello>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
